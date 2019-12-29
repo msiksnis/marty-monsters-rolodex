e6977f481acf21ecb79336d81eb18a941347bf31
marty <marty@martys-MacBook-Pro.local>
2019-12-29 14:58:13 +0100

    Updates

---
+++ precache-manifest.f3957c48bba4ec18c5fc04a672e8d6d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db078f14f1e6e9d75b856225c5f82c4c",
+    "url": "/marty-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6d18101e55e4e69def0f",
+    "url": "/marty-monsters-rolodex/static/css/main.876ee7c2.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/marty-monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/marty-monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d18101e55e4e69def0f",
+    "url": "/marty-monsters-rolodex/static/js/main.6b7844cb.chunk.js"
+  },
+  {
+    "revision": "fe0d525624fdfdc131a7",
+    "url": "/marty-monsters-rolodex/static/js/runtime-main.a1b48223.js"
+  }
+]);
